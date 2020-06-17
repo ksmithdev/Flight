a795ed75e847dcedb0689209497271b5c7bb9464
Kyle Smith <ksmith.developer@gmail.com>
2020-06-17 08:05:55 -0400

    Added the fundamental postgre implementation.

---
+++ Flight.Postgres/PostgresAuditor.cs
@@ -0,0 +1,102 @@
+﻿namespace Flight
+{
+    using Flight.Database;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Common;
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class PostgresAuditor : AuditorBase
+    {
+        private readonly string schemaName;
+        private readonly string tableName;
+
+        public PostgresAuditor(string schemaName, string tableName)
+        {
+            // TODO: check schemaName and tableName for invalid characters and throw exception to prevent a possible sql injection attack
+            this.schemaName = schemaName;
+            this.tableName = tableName;
+        }
+
+        public override async Task EnsureCreatedAsync(DbConnection connection, CancellationToken cancellationToken)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT COUNT(1) FROM pg_catalog.pg_tables WHERE schemaname=@schema;";
+                command.AddParameter("@schema", schemaName);
+
+                var result = await command.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
+                var count = Convert.ToInt32(result, CultureInfo.InvariantCulture);
+
+                if (count == 0)
+                {
+                    command.Parameters.Clear();
+                    command.CommandText = $"CREATE SCHEMA [{schemaName}];";
+                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT COUNT(1) FROM pg_catalog.pg_tables WHERE schemaname=@schema AND tablename=@table;";
+
+                command.AddParameter("@schema", schemaName);
+                command.AddParameter("@table", tableName);
+
+                var result = await command.ExecuteScalarAsync(cancellationToken).ConfigureAwait(false);
+                var count = Convert.ToInt32(result, CultureInfo.InvariantCulture);
+
+                if (count == 0)
+                {
+                    command.Parameters.Clear();
+                    command.CommandText = $@"CREATE TABLE [{schemaName}].[{tableName}] (
+    script_name varchar(255) NOT NULL,
+    checksum char(44) NOT NULL,
+    idempotent bool NOT NULL,
+    applied timestamptz NOT NULL,
+    applied_by varchar(104) NOT NULL
+);";
+
+                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                }
+            }
+        }
+
+        public override async Task<IEnumerable<AuditEntry>> LoadEntriesAsync(DbConnection connection, CancellationToken cancellationToken)
+        {
+            var auditEntries = new List<AuditEntry>();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = $"SELECT script_name, checksum, idempotent, applied, applied_by FROM [{schemaName}].[{tableName}]";
+
+            using var reader = await command.ExecuteReaderAsync(cancellationToken).ConfigureAwait(false);
+            while (await reader.ReadAsync(cancellationToken).ConfigureAwait(false))
+            {
+                auditEntries.Add(new AuditEntry
+                {
+                    ScriptName = reader.GetString(0),
+                    Checksum = reader.GetString(1),
+                    Idempotent = reader.GetBoolean(2),
+                    Applied = DateTime.SpecifyKind(reader.GetDateTime(3), DateTimeKind.Utc),
+                    AppliedBy = reader.GetString(4)
+                });
+            }
+
+            return auditEntries;
+        }
+
+        public override async Task StoreEntryAsync(DbConnection connection, DbTransaction? transaction, IScript script, CancellationToken cancellationToken)
+        {
+            using var command = connection.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandText = $"INSERT INTO [{schemaName}].[{tableName}] (script_name, checksum, idempotent, applied, applied_by) VALUES (@scriptName, @checksum, @idempotent, now(), current_user())";
+            command.AddParameter("@scriptName", script.ScriptName);
+            command.AddParameter("@checksum", script.Checksum);
+            command.AddParameter("@idempotent", script.Idempotent);
+
+            await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Flight.Postgres/PostgresMigrationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Flight
+{
+    using System;
+
+    public static class PostgresMigrationBuilderExtensions
+    {
+        public static MigrationBuilder UsePostgres(this MigrationBuilder migrationBuilder, string connectionString, string auditSchema = "flight", string auditTable = "change_sets")
+        {
+            if (migrationBuilder == null)
+                throw new ArgumentNullException(nameof(migrationBuilder));
+
+            var connectionFactory = new PostgresConnectionFactory(connectionString);
+            var batchManager = new PostgresBatchManager();
+            var auditor = new PostgresAuditor(auditSchema, auditTable);
+
+            migrationBuilder.SetConnectionFactory(connectionFactory);
+            migrationBuilder.SetBatchManager(batchManager);
+            migrationBuilder.SetAuditor(auditor);
+
+            return migrationBuilder;
+        }
+
+        public static MigrationBuilder UsePostgres(this MigrationBuilder migrationBuilder, string host, string database, string auditSchema = "flight", string auditTable = "change_sets")
+        {
+            if (migrationBuilder == null)
+                throw new ArgumentNullException(nameof(migrationBuilder));
+
+            var connectionFactory = new PostgresConnectionFactory(host, database);
+            var batchManager = new PostgresBatchManager();
+            var auditor = new PostgresAuditor(auditSchema, auditTable);
+
+            migrationBuilder.SetConnectionFactory(connectionFactory);
+            migrationBuilder.SetBatchManager(batchManager);
+            migrationBuilder.SetAuditor(auditor);
+
+            return migrationBuilder;
+        }
+    }
+}
