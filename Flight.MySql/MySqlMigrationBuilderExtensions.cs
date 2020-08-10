6fdb72824314bc19782f0d442165dea775973bd3
Kyle Smith <ksmith.developer@gmail.com>
2020-08-10 11:40:34 -0400

    Added the MySql code

---
+++ Flight.MySql/MySqlMigrationBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Flight
+{
+    using System;
+
+    public static class MySqlMigrationBuilderExtensions
+    {
+        public static MigrationBuilder UseMySql(this MigrationBuilder migrationBuilder, string connectionString, string auditTable = "ChangeSets")
+        {
+            if (migrationBuilder == null)
+                throw new ArgumentNullException(nameof(migrationBuilder));
+
+            var connectionFactory = new MySqlConnectionFactory(connectionString);
+            var batchManager = new MySqlBatchManager();
+            var auditor = new MySqlAuditor(auditTable);
+
+            migrationBuilder.SetConnectionFactory(connectionFactory);
+            migrationBuilder.SetBatchManager(batchManager);
+            migrationBuilder.SetAuditor(auditor);
+
+            return migrationBuilder;
+        }
+
+        public static MigrationBuilder UseMySql(this MigrationBuilder migrationBuilder, string server, string database, string auditTable = "ChangeSets")
+        {
+            if (migrationBuilder == null)
+                throw new ArgumentNullException(nameof(migrationBuilder));
+
+            var connectionFactory = new MySqlConnectionFactory(server, database);
+            var batchManager = new MySqlBatchManager();
+            var auditor = new MySqlAuditor(auditTable);
+
+            migrationBuilder.SetConnectionFactory(connectionFactory);
+            migrationBuilder.SetBatchManager(batchManager);
+            migrationBuilder.SetAuditor(auditor);
+
+            return migrationBuilder;
+        }
+
+        public static MigrationBuilder UseMySql(this MigrationBuilder migrationBuilder, string server, string database, string userId, string password, string auditTable = "ChangeSets")
+        {
+            if (migrationBuilder == null)
+                throw new ArgumentNullException(nameof(migrationBuilder));
+
+            var connectionFactory = new MySqlConnectionFactory(server, database, userId, password);
+            var batchManager = new MySqlBatchManager();
+            var auditor = new MySqlAuditor(auditTable);
+
+            migrationBuilder.SetConnectionFactory(connectionFactory);
+            migrationBuilder.SetBatchManager(batchManager);
+            migrationBuilder.SetAuditor(auditor);
+
+            return migrationBuilder;
+        }
+    }
+}
---
+++ FlightSample/MySql/Migrations/20190927_Initialize.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Employees (
+	EmployeeNumber VARCHAR(15) NOT NULL,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	CONSTRAINT PK_Employees PRIMARY KEY (EmployeeNumber)
+);
+
+CREATE TABLE ProjectTypes (
+	ProjectTypeId INT NOT NULL,
+	DisplayName VARCHAR(15) NOT NULL,
+	CONSTRAINT PK_ProjectTypes PRIMARY KEY (ProjectTypeId)
+);
+
+CREATE TABLE Projects (
+	ProjectNumber VARCHAR(15) NOT NULL,
+	ProjectTypeId INT NOT NULL,
+	ProjectManagerNumber VARCHAR(15) NOT NULL,
+	CONSTRAINT PK_Projects PRIMARY KEY (ProjectNumber),
+	CONSTRAINT FK_Projects_ProjectTypes FOREIGN KEY (ProjectTypeId) REFERENCES ProjectTypes(ProjectTypeId),
+	CONSTRAINT FK_Projects_Employees FOREIGN KEY (ProjectManagerNumber) REFERENCES Employees(EmployeeNumber)
+);
+
+INSERT INTO ProjectTypes (ProjectTypeId, DisplayName) VALUES (1, 'Standard');
+INSERT INTO ProjectTypes (ProjectTypeId, DisplayName) VALUES (2, 'T&M');
