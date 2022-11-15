401fb1cafa8f3dc539655cea0d470dfb61ec5f67
Kyle Smith <ksmith.developer@gmail.com>
2022-11-15 10:02:15 -0500

    Update SA1000 spacing rule

--- Flight/GlobalSuppressions.cs
+++ Flight/GlobalSuppressions.cs
@@ -2,3 +2,4 @@
 
 [assembly: SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "Scripts are all loaded from external sources and have an inherent risk.")]
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "License file holds required information.")]
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1000:Keywords should be spaced correctly", Justification = "Modern dotnet allows the new new() shorthand.")]
